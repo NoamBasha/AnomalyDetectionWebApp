0caa455fb87e55e16ca31aab145b659c4f5dfb4c
Noam Basha Desktop <noamba10@gmail.com>
2021-05-18 16:25:52 +0300

    Reformatted files.

--- Controller/server.js
+++ Controller/server.js
@@ -1,35 +1,37 @@
 //imports modules
-const express = require('express')
-const fileUpload = require('express-fileupload')
-const model = require('../Model/SearchInFile')
-const app = express()
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const model = require("../Model/SearchInFile");
+const app = express();
 
 //define app uses
-app.use(express.urlencoded({
-    extended: false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(fileUpload({}))
-app.use(express.static('../View'))
+app.use(fileUpload({}));
+app.use(express.static("../View"));
 
 //Get Method for '/' url
-app.get('/', (req, res) => {
-    res.sendFile('index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
 
 //Post Method for '/search' url
-app.post('/detect', (req, res) => {
-    // Extract train_csv_file, test_csv_file, algorithm_type.
-    // let result / anomalies = model.detect()
-    res.write('searching for ' + req.body.key + +':\n')
-    let key = req.body.key
-    if (req.files) {
-        let file = req.files.text_file
-        let result = model.searchText(key, file.data.toString())
-        res.write(result)
-    }
-    res.end()
-})
+app.post("/detect", (req, res) => {
+  // Extract train_csv_file, test_csv_file, algorithm_type.
+  // let result / anomalies = model.detect()
+  res.write("searching for " + req.body.key + +":\n");
+  let key = req.body.key;
+  if (req.files) {
+    let file = req.files.text_file;
+    let result = model.searchText(key, file.data.toString());
+    res.write(result);
+  }
+  res.end();
+});
 
 //starting server on port 8080
-app.listen(8080, ()=>console.log("server started at 8080"))
+app.listen(8080, () => console.log("server started at 8080"));
--- Model/SearchInFile.js
+++ Model/SearchInFile.js
@@ -1,10 +1,9 @@
 function searchText(key, text) {
-    let result = ''
-    text.split("\n").forEach(row => {
-        if (row.search(key) !== -1)
-            result += row + '\n'
-    })
-    return result
+  let result = "";
+  text.split("\n").forEach((row) => {
+    if (row.search(key) !== -1) result += row + "\n";
+  });
+  return result;
 }
 //export file to module
-module.exports.searchText = searchText
+module.exports.searchText = searchText;
